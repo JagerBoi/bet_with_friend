f26c97cdeb196be4bf7b20f50087fce51525d96a
H.Y.Lin <linhaoyang113@gmail.com>
2024-06-03 21:56:57 -0600

    Fetching data in front-end
    Fetching data in front-end and displaying data in front-end.

--- bwf-api/api/urls.py
+++ bwf-api/api/urls.py
@@ -1,11 +1,10 @@
 from api import views
 from rest_framework import routers
-from django.urls import path
-from django.urls import re_path, include
+from django.urls import re_path as url, include
 
 router = routers.DefaultRouter()
 router.register(r'groups',views.GroupViewset)
 
 urlpatterns = [
-    path(r'^', include(router.urls)),
+    url(r'^', include(router.urls)),
 ]
